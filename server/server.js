127586e2b66304969788749d18af08e6a459c4e5
salceson <ciolczyk.michal.93@gmail.com>
2015-03-13 22:15:55 +0100

    First stages of server.

--- server/server.js
+++ server/server.js
@@ -8,14 +8,52 @@ var config = require("./config.json"),
 
 var users = [];
 
+var state = 0;
+
 console.log("Server started");
 
-io.on('connection', function(socket){
-    username = "";
+io.on('connection', function (socket) {
+    var username = "";
     socket.emit("troll", {troll: true});
-    socket.on('join', function(data){
+    socket.on("join", function (data) {
         username = data;
         console.log(username);
+        if (state == 0) {
+            users[0] = {username: username, socket: socket};
+            socket.emit("user", 0);
+            state = 1;
+        } else if (state == 1) {
+            users[1] = {username: username, socket: socket};
+            socket.emit("user", 1);
+            state = 2;
+            downloadAuctions();
+        } else {
+            console.log("error");
+            socket.close();
+        }
     });
-    //socket.on
+    socket.on("results", function (data) {
+        if (state != 3 && state != 4) {
+            console.log("error");
+            socket.close();
+            return;
+        }
+        users[data.user].results = data.result;
+        if(state == 3)
+            state = 4;
+        else {
+            state = 5;
+            var match = ~(users[0].results ^ users[1].results);
+            for(var i=0; i<2; i++){
+                users[i].socket.emit("matches", true);
+            }
+        }
+    });
 });
+
+function downloadAuctions(){
+    var auctions = {};
+    for(var i=0; i<2; i++){
+        users[i].socket.emit("auctions", auctions);
+    }
+}

01e28e60b824a1a1ca55acda412c324cb184d467
nnkps <aniakups@gmail.com>
2015-03-13 23:24:49 +0100

    Setting up sockets on client

--- client/android/MatchMyBet/app/src/main/java/mobi/matchmybet/MainActivity.java
+++ client/android/MatchMyBet/app/src/main/java/mobi/matchmybet/MainActivity.java
@@ -1,17 +1,36 @@
 package mobi.matchmybet;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
-public class MainActivity extends ActionBarActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class MainActivity extends Activity {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://chat.socket.io");
+        } catch (URISyntaxException e) {}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSocket.connect();
     }
 
 
@@ -36,4 +55,41 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void findPair(View view) {
+        EditText editText = (EditText) findViewById(R.id.nick);
+        String nick = editText.getText().toString();
+
+        if (TextUtils.isEmpty(nick)) {
+            return;
+        }
+
+        mSocket.emit("pair_requested", nick);
+
+        mSocket.on("paired", onPaired);
+
+
+    }
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object.. args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e) {
+                        return;
+                    }
+
+                    // add the message to view
+                    addMessage(username, message);
+                }
+            });
+        }
+    };
 }

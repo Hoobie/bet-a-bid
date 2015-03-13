c0146309396c8be963ccb8ae0fc76eeac0d3c1b3
salceson <ciolczyk.michal.93@gmail.com>
2015-03-13 20:19:02 +0100

    Some tests.

--- server/server.js
+++ server/server.js
@@ -6,4 +6,16 @@
 var config = require("./config.json"),
     io = require("socket.io")(config.port);
 
-console.log("Bangla");
+var users = [];
+
+console.log("Server started");
+
+io.on('connection', function(socket){
+    username = "";
+    socket.emit("troll", {troll: true});
+    socket.on('join', function(data){
+        username = data;
+        console.log(username);
+    });
+    //socket.on
+});
